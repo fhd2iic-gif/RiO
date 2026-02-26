a1b10b4205afb46fcc5b48a339d3183b992e4196
fhd2iic-gif <fhd.2iic@gmail.com>
2026-02-26 07:54:34 +0300

    Create app/page.tsx file

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeroSection from './components/HeroSection';
+import FeaturedAssets from './components/FeaturedAssets';
+
+const HomePage = () => {
+    return (
+        <div>
+            <HeroSection />
+            <FeaturedAssets />
+        </div>
+    );
+};
+
+export default HomePage;
